9b2b4bdb1a7cc9cda9373de30e82de1015156bd6
almogch <almog@intezer.com>
2025-02-19 22:52:07 +0200

    feat(all): extension and improvements to the base xdr sdk

--- cortex_xdr_client/api/models/action_status.py
+++ cortex_xdr_client/api/models/action_status.py
@@ -2,15 +2,17 @@ from typing import Dict, Optional, Union
 
 from pydantic import BaseModel
 
+from cortex_xdr_client.api.models.base import CustomBaseModel
 
-class ActionStatuStr(BaseModel):
+
+class ActionStatuStr(CustomBaseModel):
     # Since we don't know what the returned key of <agent ID>/<endpoint ID> will be.
     __root__: Dict[Union[str, None], Union[str, None]]
 
 
-class GetActionStatusItem(BaseModel):
+class GetActionStatusItem(CustomBaseModel):
     data: Optional[ActionStatuStr]
 
 
-class GetActionStatus(BaseModel):
+class GetActionStatus(CustomBaseModel):
     reply: GetActionStatusItem
